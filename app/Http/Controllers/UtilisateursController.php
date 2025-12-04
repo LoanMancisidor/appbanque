99839e357770f63e18744cc8764b2e70f6236a47
Balkhubal <baptiste86@hotma*il.com>
2025-12-04 14:33:53 +0100

    fix env

--- app/Http/Controllers/UtilisateursController.php
+++ app/Http/Controllers/UtilisateursController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Utilisateur;
 
-class UtilisateurController extends Controller
+class UtilisateursController extends Controller
 {
     public function create()
     {
